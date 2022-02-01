ed05f9736913627923dda5dadb5cfc6515de4eb5
Emmanuel Dushime <64155652+dushimeemma@users.noreply.github.com>
2022-02-01 22:30:22 +0200

    Revert "CWM-2 (feature): authentication (#1)" (#2)
    This reverts commit ebe35a20db403e62deb17a2e2cd7834bbebac21c.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 import "../styles/globals.css";
 
-const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
 
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,41 +1,31 @@
-import Alert from "@mui/material/Alert";
-import CircularProgress from "@mui/material/CircularProgress";
-import { Formik } from "formik";
 import { GetServerSidePropsContext } from "next";
 import { getCsrfToken, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import * as Yup from "yup";
 
 import { getSession } from "@helpers/auth";
 
-import TextField from "@components/reusable/TextField";
-
-const validationSchema = Yup.object().shape({
-  email: Yup.string().email().required().label("Email"),
-  password: Yup.string().required().label("Password"),
-});
-
-interface User {
-  username?: string;
-  password: string;
-  email: string;
+interface ServerSideProps {
+  csrfToken: string;
 }
 
-const Login = () => {
+export default function Login({ csrfToken }: ServerSideProps) {
   const router = useRouter();
-  const [logMessage, setLogMessage] = useState<string>("");
-  const [logError, setLogError] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const callbackUrl = typeof router.query?.callbackUrl === "string" ? router.query.callbackUrl : "/private";
 
-  const handleLogin = async (data: User) => {
-    const { email, password } = data;
-    if (isLoading) {
+  async function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+
+    if (isSubmitting) {
       return;
     }
-    setIsLoading(true);
+
+    setIsSubmitting(true);
+
     const response = await signIn<"credentials">("credentials", {
       redirect: false,
       email,
@@ -43,116 +33,47 @@ const Login = () => {
       callbackUrl,
     });
     if (!response) {
-      setIsLoading(false);
-      setLogError("Received empty response from next auth");
       throw new Error("Received empty response from next auth");
     }
+
     if (!response.error) {
-      setIsLoading(false);
-      setLogMessage("Logged in success");
-      router.replace(callbackUrl);
+      // we're logged in! let's do a hard refresh to the desired url
+      window.location.replace(callbackUrl);
       return;
     }
-  };
+  }
 
   return (
-    <>
-      <div className="flex items-center justify-center w-screen h-screen bg-secondary">
-        <div className="flex flex-col justify-center items-center min-h-[50vh] w-[50vw]">
-          <span className="mt-5 text-2xl font-bold text-center">Cal.com</span>
-          <span className="my-5 text-2xl font-bold text-center">Sign in to your account</span>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={handleLogin}
-            validationSchema={validationSchema}>
-            {({ values, handleChange, handleSubmit, errors, handleBlur, touched, isValid }) => (
-              <form
-                onSubmit={handleSubmit}
-                className="flex flex-col justify-start px-10 pb-5 pt-10 items-start min-h-[38vh] w-[30vw]  rounded-sm border-secondary bg-white shadow-md">
-                <>
-                  <TextField
-                    label="Email address"
-                    type="text"
-                    name="email"
-                    inputClasses="mb-5"
-                    value={values.email}
-                    onBlur={handleBlur("email")}
-                    onChange={handleChange("email")}
-                  />
-                  {touched.email && errors.email && (
-                    <Alert severity="error" className="w-full mb-5 -mt-3">
-                      {errors.email}
-                    </Alert>
-                  )}
-                </>
-                <>
-                  <TextField
-                    label="Password"
-                    type="password"
-                    name="password"
-                    isPassword={true}
-                    url="#"
-                    value={values.password}
-                    onBlur={handleBlur("password")}
-                    onChange={handleChange("password")}
-                    inputClasses="mb-5"
-                  />
-                  {touched.password && errors.password && (
-                    <Alert severity="error" className="w-full mb-5 -mt-3">
-                      {errors.password}
-                    </Alert>
-                  )}
-                </>
-                <button
-                  type="submit"
-                  className={`w-full px-3 py-2 rounded-sm ${
-                    !isValid ? "bg-gray-100" : "bg-primary"
-                  } text-white`}
-                  disabled={!isValid}>
-                  {isLoading ? (
-                    <div className="flex items-center justify-center">
-                      <CircularProgress size={25} style={{ color: "white" }} />
-                      <span className="ml-2">Sign in</span>
-                    </div>
-                  ) : (
-                    "Sign in"
-                  )}
-                </button>
-              </form>
-            )}
-          </Formik>
+    <form onSubmit={handleSubmit}>
+      <input name="csrfToken" type="hidden" defaultValue={csrfToken || undefined} hidden />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        placeholder="email"
+        required
+        value={email}
+        onInput={(e) => setEmail(e.currentTarget.value)}
+        className="block border border-neutral-300 focus:ring-neutral-900"
+      />
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="password"
+        autoComplete="current-password"
+        required
+        value={password}
+        onInput={(e) => setPassword(e.currentTarget.value)}
+        className="block border border-neutral-300 focus:ring-neutral-900"
+      />
 
-          <div className="flex flex-row mt-5">
-            <span>Don&prime;t have an account &#63;</span>
-            <a href="/auth/signup" className="font-bold">
-              Create account
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="fixed left-[40%] bottom-10 shadow-2xl">
-        {logError && (
-          <button
-            type="button"
-            onClick={() => setLogError("")}
-            className="flex items-center justify-center w-full h-full">
-            <Alert severity="error">{logError}</Alert>
-          </button>
-        )}
-        {logMessage && (
-          <button
-            type="button"
-            onClick={() => setLogMessage("")}
-            className="flex items-center justify-center w-full h-full">
-            <Alert severity="success">{logMessage}</Alert>
-          </button>
-        )}
-      </div>
-    </>
+      <button type="submit" disabled={isSubmitting} className="p-1 text-white bg-blue-800">
+        SIGN IN
+      </button>
+    </form>
   );
-};
-
-export default Login;
+}
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const { req } = context;
